1a5f78b12d714dec3e4d3e5096f122a211a12a2a
Maximous Hilal <maximous.hilal@gmail.com>
2019-03-01 15:41:47 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+    'use strict';
+    document.querySelector(".bar").addEventListener("click", function () {
+        let nav = document.querySelector(".main-nav");
+        nav.classList.toggle("hidden");
+        nav.classList.toggle("slideInDown");
+    });
+    
+    document.querySelector('.scroll-down').addEventListener('click', function () {
+        window.scrollBy(0, window.innerHeight);
+    })
+};
